9dea8fa9357cbc09a03437cc9bddbf6a5926db69
JunShen <tony88010955@gmail.com>
2023-08-12 16:18:52 +0800

    fetch

--- app.js
+++ app.js
@@ -1,38 +1,53 @@
-const btn = document.querySelector("button");
+// const btn = document.querySelector("button");
 const url = "./api/peoples.json";
-btn.addEventListener("click", () => getData(url));
+// btn.addEventListener("click", () => getData(url));
 
-// it will not cause web refresh!! (importance)
-const getData = (url) => {
-  // run in background
-  const xhr = new XMLHttpRequest();
+// // it will not cause web refresh!! (importance)
+// const getData = (url) => {
+//   // run in background
+//   const xhr = new XMLHttpRequest();
 
-  xhr.open("GET", url);
-  xhr.onreadystatechange = () => {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      const data = JSON.parse(xhr.responseText);
-      const displayData = data
-        .map((item) => {
-          return `<p>${item.name}<p>`;
-        })
-        .join("");
-      const element = document.createElement("div");
-      element.innerHTML = displayData;
-      document.body.appendChild(element);
-    } else {
-      console.log({
-        status: xhr.status,
-        text: xhr.statusText,
-        readyState: xhr.readyState,
-      });
-    }
-  };
-  xhr.send();
-  // run in background
-};
+//   xhr.open("GET", url);
+//   xhr.onreadystatechange = () => {
+//     if (xhr.readyState === 4 && xhr.status === 200) {
+//       const data = JSON.parse(xhr.responseText);
+//       const displayData = data
+//         .map((item) => {
+//           return `<p>${item.name}<p>`;
+//         })
+//         .join("");
+//       const element = document.createElement("div");
+//       element.innerHTML = displayData;
+//       document.body.appendChild(element);
+//     } else {
+//       console.log({
+//         status: xhr.status,
+//         text: xhr.statusText,
+//         readyState: xhr.readyState,
+//       });
+//     }
+//   };
+//   xhr.send();
+//   // run in background
+// };
 
-// it is asynchronous function, too.
-setTimeout(() => {
-  console.log("hello world");
-}, 5000);
-console.log("log first!!");
+// // it is asynchronous function, too.
+// setTimeout(() => {
+//   console.log("hello world");
+// }, 5000);
+// console.log("log first!!");
+
+// fetch
+fetch(url)
+  .then((resp) => {
+    // useful methods and prop
+    // return promise
+    // console.log(resp);
+    return resp.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
